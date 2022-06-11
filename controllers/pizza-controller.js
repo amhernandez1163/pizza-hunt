5fe8c7d6319aeaedc78cd9046539eb48bd039146
Amanda Dell <amhernandez1163@gmail.com>
2022-06-11 10:17:45 -0500

    updated comment backend and tied to front end

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -4,18 +4,28 @@ const pizzaController = {
   // get all pizzas  - will serve as the callback function for the GET /api/pizzas route
   getAllPizza(req, res) {
     Pizza.find({})
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
+      .sort({ _id: -1 })
       .then((dbPizzaData) => res.json(dbPizzaData))
       .catch((err) => {
         console.log(err);
-        res.status(400).json(err);
+        res.sendStatus(400);
       });
   },
 
   // get one pizza by id in the URL
   getPizzaById({ params }, res) {
     Pizza.findOne({ _id: params.id })
+      .populate({
+        path: "comments",
+        select: "-__v",
+      })
+      .select("-__v")
       .then((dbPizzaData) => {
-        // If no pizza is found, send 404
         if (!dbPizzaData) {
           res.status(404).json({ message: "No pizza found with this id!" });
           return;
